da8b86722e10eb4fb21f90e77d0657cb2b131348
Andrew Russell <a>
2019-05-18 10:17:49 -0600

    Adding solution for Two Strings

---
+++ HankerRankSolutions/Hashes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HankerRankSolutions
+{
+    public static class HashesProblems
+    {
+        // Complete the twoStrings function below.
+        public static string twoStrings(string s1, string s2)
+        {
+            if (s1.Length > s2.Length) {
+                return StringCompare(s1, s2);
+            }
+            else
+            {
+                return StringCompare(s2, s1);
+            }
+        }
+
+        public static string StringCompare(string s1, string s2)
+        {
+            var s1Hash = new HashSet<char>(s1);
+
+            for (int i = 0; i < s2.Length; i++)
+            {
+                if (s1Hash.Contains(s2[i]))
+                {
+                    return "YES";
+                }
+            }
+            return "NO";
+        }
+    }
+}
---
+++ HankerRankSolutionsTests/HashesProblemsTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HankerRankSolutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HankerRankSolutions.Tests
+{
+    [TestClass()]
+    public class HashesProblemsTests
+    {
+        [TestMethod()]
+        public void twoStringsTest_Example1()
+        {
+            string s1 = "a";
+            string s2 = "art";
+
+            string results = HashesProblems.twoStrings(s1, s2);
+
+            Assert.IsTrue("YES" == results);
+        }
+        [TestMethod()]
+        public void twoStringsTest_Example2()
+        {
+            string s1 = "be";
+            string s2 = "cat";
+
+            string results = HashesProblems.twoStrings(s1, s2);
+
+            Assert.IsTrue("NO" == results);
+        }
+        [TestMethod()]
+        public void twoStringsTest_Example3()
+        {
+            string s1 = "hello";
+            string s2 = "world";
+
+            string results = HashesProblems.twoStrings(s1, s2);
+
+            Assert.IsTrue("YES" == results);
+        }
+        [TestMethod()]
+        public void twoStringsTest_Example4()
+        {
+            string s1 = "hi";
+            string s2 = "world";
+
+            string results = HashesProblems.twoStrings(s1, s2);
+
+            Assert.IsTrue("NO" == results);
+        }
+    }
+}
