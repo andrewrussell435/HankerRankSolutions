7374ac9ddd36d174def7260c0610431f2dc756d7
Andrew Russell <a>
2019-05-21 19:29:36 -0600

    Adding solution for Alternating Characters

---
+++ HankerRankSolutions/Strings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HankerRankSolutions
+{
+    public static class StringManipulationProblems
+    {
+        //solution for Alternating Characters
+        public static int alternatingCharacters(string s)
+        {
+            int deletionCount = 0;
+
+            char lastChar = '0';
+            foreach (var c in s)
+            {
+                if (c != lastChar)
+                {
+                    lastChar = c;
+                }
+                else
+                {
+                    deletionCount++;
+                }
+            }
+
+            return deletionCount;
+
+        }
+    }
+}
---
+++ HankerRankSolutionsTests/StringManipulationProblemsTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HankerRankSolutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HankerRankSolutions.Tests
+{
+    [TestClass()]
+    public class StringManipulationProblemsTests
+    {
+        [TestMethod()]
+        public void alternatingCharactersTest_SampleOne()
+        {
+            string s = "AAAA";
+            var results = StringManipulationProblems.alternatingCharacters(s);
+            Assert.AreEqual(3, results);
+        }
+        [TestMethod()]
+        public void alternatingCharactersTest_SampleTwo()
+        {
+            string s = "BBBBB";
+            var results = StringManipulationProblems.alternatingCharacters(s);
+            Assert.AreEqual(4, results);
+        }
+        [TestMethod()]
+        public void alternatingCharactersTest_SampleThree()
+        {
+            string s = "ABABABAB";
+            var results = StringManipulationProblems.alternatingCharacters(s);
+            Assert.AreEqual(0, results);
+        }
+        [TestMethod()]
+        public void alternatingCharactersTest_SampleFour()
+        {
+            string s = "BABABA";
+            var results = StringManipulationProblems.alternatingCharacters(s);
+            Assert.AreEqual(0, results);
+        }
+        [TestMethod()]
+        public void alternatingCharactersTest_SampleFive()
+        {
+            string s = "AAABBB";
+            var results = StringManipulationProblems.alternatingCharacters(s);
+            Assert.AreEqual(4, results);
+        }
+    }
+}
