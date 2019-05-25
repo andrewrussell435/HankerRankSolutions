5199f19e8e8694a8bdbccf52bc649431efa31a3f
Andrew Russell <a>
2019-05-25 16:46:50 -0600

    Adding solution for Mark And Toys

---
+++ HankerRankSolutions/Sorting.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HankerRankSolutions
+{
+    public static class SortingProblems
+    {
+
+        //Solution for toys and mark
+        public static int maximumToys(int[] prices, int k)
+        {
+            int toysCount = 0;
+
+            QuickSort(prices, 0, prices.Length-1);
+
+            int totalPrice = 0;
+
+            while (totalPrice <= k)
+            {
+                totalPrice += prices[toysCount];
+                if (totalPrice < k)             {
+                    toysCount++;
+                }
+            }
+
+            return toysCount;
+
+        }
+
+        public static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left >= right)
+            {
+                return;
+            }
+            else
+            {
+                int pivot = arr[(left+right) / 2];
+                int index = Partition(arr, left, right, pivot);
+                QuickSort(arr, left, index - 1);
+                QuickSort(arr, index, right);
+            }
+        }
+
+        public static int Partition(int[] arr, int left, int right, int pivot)
+        {
+            while (left <= right)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+                if (left <= right) 
+                {
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                    left++;
+                    right--;
+                }
+            }
+            return left;
+
+        }
+    }
+}
---
+++ HankerRankSolutionsTests/SortingProblemsTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HankerRankSolutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HankerRankSolutions.Tests
+{
+    [TestClass()]
+    public class SortingProblemsTests
+    {
+        [TestMethod()]
+        public void maximumToysTest_SampleOne()
+        {
+            int totalMoney = 50;
+            var toyPrices = new int[] { 1, 12, 5, 111, 200, 1000, 10 };
+
+            var results = SortingProblems.maximumToys(toyPrices, totalMoney);
+            Assert.AreEqual(4, results);
+        }
+        [TestMethod()]
+        public void maximumToysTest_SampleTwo()
+        {
+            int totalMoney = 7;
+            var toyPrices = new int[] { 1, 2, 3, 4 };
+
+            var results = SortingProblems.maximumToys(toyPrices, totalMoney);
+            Assert.AreEqual(3, results);
+        }
+        [TestMethod()]
+        public void maximumToysTest_SampleThree()
+        {
+            int totalMoney = 15;
+            var toyPrices = new int[] { 3, 7, 2, 9, 4 };
+
+            var results = SortingProblems.maximumToys(toyPrices, totalMoney);
+            Assert.AreEqual(3, results);
+        }
+    }
+}
