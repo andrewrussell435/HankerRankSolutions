9513c717acf7c6b41fe85b796175e6f27d410968
AndrewRussell435 <andrewrussell435@gmail.com>
2019-10-17 15:14:52 -0600

    f# alternating characters -- refactored code to no longer depend on passing a character around

--- HackerRankFsharpSolutions/Strings.fs
+++ HackerRankFsharpSolutions/Strings.fs
@@ -4,18 +4,31 @@ module StringManipulationProblemsFsharp =
     //Solution for Alternating Characters
     //https://www.hackerrank.com/challenges/alternating-characters/problem
 
-    //Attempt 2
-    let rec getAlternatingCharacterString (c : char) (testString : string ) : string = 
-        if testString.Length = 0 then
-            c.ToString()
-        elif c = testString.[0] then
-            getAlternatingCharacterString c testString.[1..testString.Length-1]
+    //Attempt 3
+    let rec getAlternatingCharacterString (testString : string ) : string = 
+        if testString.Length = 1 then
+            testString
+        elif testString.[0] = testString.[1] then
+            getAlternatingCharacterString testString.[1..testString.Length-1]
         else
-            c.ToString() + getAlternatingCharacterString testString.[0] testString.[1..testString.Length-1]
+            testString.[0].ToString() + getAlternatingCharacterString testString.[1..testString.Length-1]
 
     let alternatingCharacters (testString : string) =
-        let testStringWithOnlyAlternatingCharacters = getAlternatingCharacterString testString.[0] testString.[1..testString.Length-1]
+        let testStringWithOnlyAlternatingCharacters = getAlternatingCharacterString testString
         testString.Length - testStringWithOnlyAlternatingCharacters.Length
+
+    ////Attempt 2
+    //let rec getAlternatingCharacterString (c : char) (testString : string ) : string = 
+    //    if testString.Length = 0 then
+    //        c.ToString()
+    //    elif c = testString.[0] then
+    //        getAlternatingCharacterString c testString.[1..testString.Length-1]
+    //    else
+    //        c.ToString() + getAlternatingCharacterString testString.[0] testString.[1..testString.Length-1]
+
+    //let alternatingCharacters (testString : string) =
+    //    let testStringWithOnlyAlternatingCharacters = getAlternatingCharacterString testString.[0] testString.[1..testString.Length-1]
+    //    testString.Length - testStringWithOnlyAlternatingCharacters.Length
         
     //Attempt 1
     //let alternatingCharacters (name : string) =
